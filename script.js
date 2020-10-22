8b6982bfb21dbdf2cf2508a6953ca2a949d39901
James Tuck <JamesjTuck@gmail.com>
2020-10-21 19:21:39 -0600

    Added array of utah resorts and a for loop

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
+var skiResorts = ['snowbird'
+    , 'alta'
+    , 'brianhead'
+    , 'brighton'
+    , 'deer-valley'
+    , 'parkcity'
+    , 'solitude'];
+function liftieTest(arr) {
+    for(var i = 0; i < arr.length; i++){
 
-function liftieTest(){
-
-$.ajax({
-    url: "https://cors-anywhere.herokuapp.com/https://liftie.info/api/resort/Park%20City%20Mountain",
-    method: "GET"
-}).then(function(response) {
-      console.log(response);
-});
+    $.ajax({
+        url: "https://cors-anywhere.herokuapp.com/https://liftie.info/api/resort/" + arr[i],
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+}
 };
 
-liftieTest();
+liftieTest(skiResorts);
 
 
 
 
-function mapsTest(){
+function mapsTest() {
     var apiKey = 'AIzaSyCp8r9ykeHiZkwSGPBhA6nn5ZDjvlOUScc';
     var queryURL = 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=ski%20resort&inputtype=textquery&locationbias=circle:100000@40.3896709,-111.8292264&fields=name,formatted_address&key=' + apiKey
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-      console.log(response);
-});
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
 };
 
 // mapsTest();
