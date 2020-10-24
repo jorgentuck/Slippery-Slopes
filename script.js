0d490acdd3d64cbc211f3e14a7059f3355219f7d
James Tuck <JamesjTuck@gmail.com>
2020-10-24 10:09:29 -0600

    lifite call now first

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ $(document).ready(function () {
         , 'brighton'
         , 'deer-valley'
         , 'parkcity'
-        , 'solitude'];
+        , 'solitude'
+        , 'snowbasin'];
 
     var userLat = 0;
     var userLon = 0;
     var lat = 0;
     var lon = 0;
     var index = 0;
+    var mapIndex = 0;
     var resortObj = [];
 
     var cardEl = $('#cards');
@@ -27,7 +29,7 @@ $(document).ready(function () {
                 url: "https://cors-anywhere.herokuapp.com/https://liftie.info/api/resort/" + arr[i],
                 method: "GET"
             }).then(function (response) {
-                
+
 
                 lat = response.ll[1];
                 lon = response.ll[0];
@@ -45,12 +47,13 @@ $(document).ready(function () {
                         open: response.lifts.open,
                         closed: response.lifts.closed,
                         liftStatus: response.lifts.status
-                    }                    
+                    }
                 })
                 // console.log(resortObj[index].name);
                 // console.log(index + " - 7 7 times");
-                mapsTest(index);
+                $('#btn' + (index + 1)).text((resortObj[index].name.toString()));
                 index++;
+                console.log(index)
             });
             // console.log(resortObj);
         }
@@ -61,26 +64,30 @@ $(document).ready(function () {
 
 
 
-    function mapsTest(i) {
-        var apiKey = secrets.GOOGLE_API_KEY;
+    function mapsTest(arr) {
+        // var apiKey = secrets.GOOGLE_API_KEY;
+        var apiKey = 'AIzaSyCRq16GUXPHn6KxkuSR0X811OTyPaJQ4c0';
         var queryURL = 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin=' + userLat + ',' + userLon + '&destination=' + lat + ',' + lon + '&key=' + apiKey
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            
-            resortObj[i].destinationlat = response.routes[0].legs[0].end_location.lat;
-            resortObj[i].destinationlon = response.routes[0].legs[0].end_location.lng;
-            resortObj[i].distanceText = response.routes[0].legs[0].distance.text;
-            resortObj[i].distanceValue = response.routes[0].legs[0].distance.value;
-            resortObj[i].durationText = response.routes[0].legs[0].duration.text;
-            resortObj[i].durationValue = response.routes[0].legs[0].duration.value;
-            if(i === skiResorts.length - 1) {
+        for (var i = 0; i < arr.length; i++) {
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
 
-            }
-        });
-        
+                resortObj[mapIndex].destinationlat = response.routes[0].legs[0].end_location.lat;
+                resortObj[mapIndex].destinationlon = response.routes[0].legs[0].end_location.lng;
+                resortObj[mapIndex].distanceText = response.routes[0].legs[0].distance.text;
+                resortObj[mapIndex].distanceValue = response.routes[0].legs[0].distance.value;
+                resortObj[mapIndex].durationText = response.routes[0].legs[0].duration.text;
+                resortObj[mapIndex].durationValue = response.routes[0].legs[0].duration.value;
+
+                mapIndex++
+                if (i === skiResorts.length - 1) {
+
+                }
+            });
+        }
     };
 
     // mapsTest();
@@ -112,13 +119,14 @@ $(document).ready(function () {
     function setPosition(position) {
         userLat = position.coords.latitude;
         userLon = position.coords.longitude;
-        liftieTest(skiResorts);
+        // liftieTest(skiResorts);
+        mapsTest(resortObj);
     }
 
-    $('.loc-link').on('click', function(event){
+    $('.loc-link').on('click', function (event) {
         cardEl.removeClass('d-none');
         getLocation();
     })
-    
+    liftieTest(skiResorts);
 
 });
